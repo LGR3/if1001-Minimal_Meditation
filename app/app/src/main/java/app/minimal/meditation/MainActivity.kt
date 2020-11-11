3e965fe31bce27af3f0042efc272ebd3d27e4791
mmp-s <mmps@cin.ufpe.br>
2020-11-11 17:53:36 -0300

    get device location on countdown end

--- app/app/src/main/java/app/minimal/meditation/MainActivity.kt
+++ app/app/src/main/java/app/minimal/meditation/MainActivity.kt
@@ -1,15 +1,54 @@
 package app.minimal.meditation
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.location.Geocoder
 import android.os.Bundle
 import android.os.CountDownTimer
+import android.util.Log
 import android.view.View
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
+    private lateinit var currentLocation: String
+    private val permissionCode = 101
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        fusedLocationProviderClient =  LocationServices.getFusedLocationProviderClient(this@MainActivity)
+        fetchLocation()
+    }
+
+    private fun fetchLocation() {
+        if (ActivityCompat.checkSelfPermission(
+                this, Manifest.permission.ACCESS_FINE_LOCATION) !=
+            PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                this, Manifest.permission.ACCESS_COARSE_LOCATION) !=
+            PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), permissionCode)
+            return
+        }
+        val geo = Geocoder(applicationContext)
+        val task = fusedLocationProviderClient.lastLocation
+        task.addOnSuccessListener { loc ->
+            if (loc != null) {
+                val location_list = geo.getFromLocation(
+                    loc.latitude, loc.longitude, 1
+                )
+                val city: String = location_list[0].locality
+                val state: String = location_list[0].adminArea
+                val country: String = location_list[0].countryName
+                currentLocation = String.format("%s, %s, %s", city, state, country)
+            }
+        }
     }
 
     fun startTimer(view: View) {
@@ -23,6 +62,7 @@ class MainActivity : AppCompatActivity() {
             }
             override fun onFinish() {
                 val endStr: String = getString(R.string.end_time)
+                Log.d("info", currentLocation)
                 startBtn.text = endStr
             }
         }.start()
