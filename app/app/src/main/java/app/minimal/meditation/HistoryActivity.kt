fdfac8594baf09e9512ccb51e484a37deffbb1a2
mmp-s <mmps@cin.ufpe.br>
2020-11-29 07:44:55 -0300

    add history activity

--- app/app/src/main/java/app/minimal/meditation/HistoryActivity.kt
+++ app/app/src/main/java/app/minimal/meditation/HistoryActivity.kt
@@ -1,15 +1,66 @@
 package app.minimal.meditation
 
+import android.content.Context
 import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.LinearLayout
+import android.widget.TextView
+import kotlinx.android.synthetic.main.history_row.view.*
+import java.text.SimpleDateFormat
+import java.util.*
+import kotlin.time.seconds
 
 class HistoryActivity : BaseActivity() {
+    private lateinit var meditationDBHelper : MeditationDBHelper
+    private var parentLinearLayout: LinearLayout? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_history)
 
+        meditationDBHelper = MeditationDBHelper(this)
+        parentLinearLayout = findViewById(R.id.parent_linear_layout)
         loadMainActivity()
         loadExtendedActivity()
         loadInfoActivity()
-        loadHistoryActivity()
+        updateTotalHours()
+        fillHistory()
+    }
+
+    private fun updateTotalHours() {
+        val total = meditationDBHelper.getTotalSeconds()
+        val totalHours: TextView = findViewById(R.id.totalHours)
+        val hours = total.toDouble() / 3600
+        totalHours.text = hours.toString()
+    }
+
+    private fun fillHistory() {
+        val allMeditations = meditationDBHelper.readAllMeditations()
+        val reversedMeditations = allMeditations.reversed()
+        reversedMeditations.forEachIndexed { index, it ->
+            val inflater = getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+            val rowView: View = inflater.inflate(R.layout.history_row, null)
+            parentLinearLayout!!.addView(rowView, parentLinearLayout!!.childCount)
+
+            val location = findViewById<TextView>(R.id.location)
+            val seconds = findViewById<TextView>(R.id.seconds)
+            val timestamp = findViewById<TextView>(R.id.timestamp)
+
+            val datetime = Date(it.timestamp.toLong() * 1000)
+            val formatter = SimpleDateFormat("dd/MM/yyyy HH:mm:ss", Locale.getDefault())
+            val formattedDate = formatter.format(datetime)
+
+            location.text = it.location
+            seconds.text = "${it.seconds}s"
+            timestamp.text = formattedDate
+
+            location.id = -1
+            seconds.id = -1
+            timestamp.id = -1
+
+        }
     }
 }
--- app/app/src/main/java/app/minimal/meditation/MeditationDBHelper.kt
+++ app/app/src/main/java/app/minimal/meditation/MeditationDBHelper.kt
@@ -7,6 +7,7 @@ import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteException
 import android.database.sqlite.SQLiteOpenHelper
 import android.provider.BaseColumns
+import android.util.Log
 
 class MeditationDBHelper(context: Context) : SQLiteOpenHelper(context, DATABASE_NAME, null, DATABASE_VERSION) {
     override fun onCreate(db: SQLiteDatabase) {
@@ -33,14 +34,31 @@ class MeditationDBHelper(context: Context) : SQLiteOpenHelper(context, DATABASE_
         return true
     }
 
+    fun getTotalSeconds(): Int {
+        var total = 0
+        val db = writableDatabase
+        var cursor: Cursor? = null
+
+        try {
+            cursor = db.rawQuery("SELECT SUM("+ DBContract.MeditationEntry.COLUMN_SECONDS  +") from " + DBContract.MeditationEntry.TABLE_NAME, null)
+        } catch (e: SQLiteException) {
+            return total
+        }
+
+        if (cursor!!.moveToFirst()) {
+            total = cursor.getInt(0)
+        }
+        cursor.close()
+        return total
+    }
+
     fun readAllMeditations(): ArrayList<MeditationModel> {
         val meditations = ArrayList<MeditationModel>()
         val db = writableDatabase
         var cursor: Cursor? = null
         try {
-            cursor = db.rawQuery("select * from " + DBContract.MeditationEntry.TABLE_NAME, null)
+            cursor = db.rawQuery("SELECT * FROM " + DBContract.MeditationEntry.TABLE_NAME + " LIMIT 50;", null)
         } catch (e: SQLiteException) {
-            db.execSQL(SQL_CREATE_ENTRIES)
             return ArrayList()
         }
 
@@ -66,7 +84,7 @@ class MeditationDBHelper(context: Context) : SQLiteOpenHelper(context, DATABASE_
         const val DATABASE_NAME = "Meditations.db"
         val SQL_DELETE_ENTRIES = "DROP TABLE IF EXISTS ${DBContract.MeditationEntry.TABLE_NAME}"
         val SQL_CREATE_ENTRIES =
-            "CREATE TABLE ${DBContract.MeditationEntry.TABLE_NAME} (" +
+            "CREATE TABLE IF NOT EXISTS ${DBContract.MeditationEntry.TABLE_NAME} (" +
                     "${BaseColumns._ID} INTEGER PRIMARY KEY," +
                     "${DBContract.MeditationEntry.COLUMN_LOCATION} TEXT," +
                     "${DBContract.MeditationEntry.COLUMN_TIMESTAMP} INTEGER," +
